e25d1e486d9e2709fc23f842aebf49af25b5df3b
Ethan Clark <elc3@students.calvin.edu>
2017-10-26 00:39:41 -0400

    Updated schema and diagrams

--- schema.sql
+++ schema.sql
@@ -7,8 +7,23 @@ drop table player_game;
 drop table event;
 drop table game;
 drop table player;
+drop table team;
 
--- Player table
+drop table vball_player_game;
+drop table vball_play;
+drop table vball_game;
+drop table vball_player;
+drop table vball_team;
+
+-- Soccer Team table
+create table team
+(
+    id integer PRIMARY KEY,
+    school_name varchar(50),
+    conference varchar(50)
+);
+
+-- Soccer Player table
 create table player
 (
     id integer PRIMARY KEY,
@@ -24,7 +39,7 @@ create table player
     red_cards integer
 );
 
--- Game table
+-- Soccer Game table
 create table game
 (
     id integer PRIMARY KEY,
@@ -32,20 +47,20 @@ create table game
     away_team varchar(30)
 );
 
--- Event table
--- Events are connected to a specific game
+-- Soccer Event table
+-- Events are connected to a specific soccer game
 -- Many events to one game (Many-to-One relationship)
 create table event
 (
     id integer PRIMARY KEY,
     game_id integer,
     team_name varchar(30),
-    time_of_event varchar(5),
-    description_event varchar(60),
+    time_of_event varchar(6),
+    description_event varchar(100),
     FOREIGN KEY (game_id) REFERENCES game(id)
 );
 
--- Player_game table
+-- Soccer Player_game table
 -- This table is needed for looking at recent player trends
 -- This connects a specific player to a specific game
 -- Many players to many games (Many-to-Many relationship)
@@ -59,4 +74,73 @@ create table player_game
     PRIMARY KEY (player_id, game_id),
     FOREIGN KEY (player_id) REFERENCES player(id),
     FOREIGN KEY (game_id) REFERENCES game(id)
+);
+
+-- Volleyball Team table
+create table vball_team
+(
+    id integer PRIMARY KEY,
+    school_name varchar(50),
+    conference varchar(50)
+);
+
+-- Volleyball Player table
+create table vball_player
+(
+    id integer PRIMARY KEY,
+    school_id integer,
+    name varchar(30),
+    year varchar(10),
+    position varchar(6),
+    matches_played integer,
+    sets_played integer,
+    kills integer,
+    errors integer,
+    attempts integer,
+    hitting_perc float,
+    assists integer,
+    service_aces integer,
+    digs integer,
+    solo_blocks integer,
+    block_assists integer,
+    points integer,
+    FOREIGN KEY (school_id) REFERENCES vball_team(id)
+);
+
+-- Volleyball Game table
+create table vball_game
+(
+    id integer PRIMARY KEY,
+    home_team integer,
+    away_team integer,
+    FOREIGN KEY (home_team) REFERENCES vball_team(id),
+    FOREIGN KEY (away_team) REFERENCES vball_team(id)
+);
+
+-- Volleyball Play table
+-- Plays are connected to a specific volleyball game
+-- Many plays to one game (Many-to-One relationship)
+create table vball_play
+(
+    id integer PRIMARY KEY,
+    game_id integer,
+    server_id integer,
+    rotation integer,
+    result varchar(60),
+    actor_id integer,
+    FOREIGN KEY (game_id) REFERENCES vball_game(id),
+    FOREIGN KEY (server_id) REFERENCES vball_player(id),
+    FOREIGN KEY (actor_id) REFERENCES vball_player(id)
+);
+
+-- Volleyball Player_game table
+-- This connects a specific player to a specific volleyball game
+-- Many players to many games (Many-to-Many relationship)
+create table vball_player_game
+(
+    player_id integer,
+    game_id integer,
+    PRIMARY KEY(player_id, game_id)
+    FOREIGN KEY (player_id) REFERENCES vball_player(id),
+    FOREIGN KEY (game_id) REFERENCES vball_game(id)
 );
